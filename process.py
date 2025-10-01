fd716808665add5a48dc5477e3c4ad9784f5d323
SabnamLaskar <104367346+SabnamLaskar@users.noreply.github.com>
2025-10-01 09:18:05 +0530

    Update process.py

--- process.py
+++ process.py
@@ -149,7 +149,7 @@ if salary_file:
     
     # Detect header row automatically
     
-    temp_df = pd.read_excel(salary_file, sheet_name="Input data(Earning & Deduction)", header=None)
+    temp_df = pd.read_excel(salary_file, header=None)
     header_row = temp_df[temp_df.apply(lambda row: row.astype(str).str.contains("Cost Centre").any(), axis=1)].index[0]
     
     df = pd.read_excel(salary_file, sheet_name="Input data(Earning & Deduction)", header=header_row)
@@ -212,3 +212,4 @@ if salary_file:
         data=output,
         file_name="processed_salary_summary.xlsx"
     )
+
